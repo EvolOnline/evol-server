18d3bb3c568e9455500d24ed724af93755c672cb
Andrei Karas <akaras@inbox.ru>
2010-11-22 22:04:53 +0200

    Allow exchange between cart and storage only for carton role.

--- src/map/clif.c
+++ src/map/clif.c
@@ -8635,7 +8635,7 @@ void clif_parse_MoveToKafraFromCart (int fd, struct map_session_data *sd)
     nullpo_retv (sd);
 
     if ((sd->npc_id != 0 && !sd->npc_flags.storage) || sd->trade_partner != 0
-        || !sd->state.storage_flag)
+        || !sd->state.storage_flag || !pc_iscarton (sd))
         return;
     if (sd->state.storage_flag == 1)
         storage_storageaddfromcart (sd, RFIFOW (fd, 2) - 2, RFIFOL (fd, 4));
@@ -8653,7 +8653,7 @@ void clif_parse_MoveFromKafraToCart (int fd, struct map_session_data *sd)
     nullpo_retv (sd);
 
     if ((sd->npc_id != 0 && !sd->npc_flags.storage) || sd->trade_partner != 0
-        || !sd->state.storage_flag)
+        || !sd->state.storage_flag || !pc_iscarton (sd))
         return;
     if (sd->state.storage_flag == 1)
         storage_storagegettocart (sd, RFIFOW (fd, 2) - 1, RFIFOL (fd, 4));
